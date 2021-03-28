5561e44f8357a6d6c957fa17d11e96f8c971181f
Supason Kotanut <supasonk@gmail.com>
2021-03-28 12:53:14 +0700

    feat | add easteregg

--- src/components/PoolTable/PoolTable.jsx
+++ src/components/PoolTable/PoolTable.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../Button/Button'
 
 export const PoolTable = ({ lp, approve, stake, isApprove }) => {
+  const [easterEgg, setEasterEgg] = useState(false)
   const headerClass = 'pt-4 pb-4'
+  const onHover = () => {
+    setEasterEgg(!easterEgg)
+  }
   return (
-    <div className='p-4 text-sm'>
+    <div className='p-4 text-sm' >
       <div className='flex border border-l-0 border-r-0 border-t-0 border-gray'>
-        <div className={`w-3/12 ${headerClass}`}>
+        <div onClick={onHover} className={`w-3/12 ${headerClass}`}>
           Pool
         </div>
         <div className={`w-2/12 ${headerClass}`}>
@@ -37,7 +41,7 @@ export const PoolTable = ({ lp, approve, stake, isApprove }) => {
         <div className={`w-3/12 ${headerClass}`}>
           Yield Farming   :    442.74%<br />
           Trading Fees    :    45.95%<br />
-          Alpaca Reward :    404.78%<br />
+          KillSwitch Reward :    404.78%<br />
         </div>
         <div className={`w-2/12 ${headerClass}`}>
           {lp}
@@ -45,12 +49,41 @@ export const PoolTable = ({ lp, approve, stake, isApprove }) => {
         <div className={`w-2/12 ${headerClass}`}>
           <div className='w-24'>
             <Button onClick={() => {isApprove ? stake() : approve() }} type='secondary'>
-                { isApprove ? 'Farm' : 'Stake' }
+                { isApprove ? 'Farm' : 'Approve' }
             </Button>
           </div>
         </div>
       </div>
-    </div>
+      {easterEgg && <div className='flex items-center' >
+      <div className={`flex w-3/12 ${headerClass}`}>
+          <div className='mr-1 relative'>
+            <img  src='/img/BSC.png' />
+            <img className='absolute top-0 right-4' src='/img/logo/killswitch.png' />
+          </div>
+          Pancakeswap<br/>
+          BNB-KillSwitch
+        </div>
+        <div className={`w-2/12 ${headerClass}`}>
+          <span className='text-secondary'>883.92% </span>
+        </div>
+        <div className={`w-3/12 ${headerClass}`}>
+          Yield Farming   :    442.74%<br />
+          Trading Fees    :    45.95%<br />
+          KillSwitch Reward :    404.78%<br />
+        </div>
+        <div className={`w-2/12 ${headerClass}`}>
+          {lp}
+        </div>
+        <div className={`w-2/12 ${headerClass}`}>
+          <div className='w-24'>
+            <Button onClick={() => {isApprove ? stake() : approve() }} type='secondary'>
+                Coming Soon
+            </Button>
+          </div>
+        </div>
+      </div>
+      }
+   </div>
   )
 }
 
