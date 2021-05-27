ecf00008175d8f959e35d6a0a9d7c3d8a2216f5c
Theme <5batman400@gmail.com>
2021-05-27 13:15:18 +0700

    fix css top auction page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './routes/index'
-import 'antd/dist/antd.css'
-import './index.css';
-import './main.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "./routes/index";
+import "antd/dist/antd.css";
+// import "./index.css";
+// import "./main.css";
+import "./m.css";
 
-
-ReactDOM.render(
-  <Router/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router />, document.getElementById("root"));
