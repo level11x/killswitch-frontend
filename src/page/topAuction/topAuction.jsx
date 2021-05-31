50103d6e9617db7ac4070af13f457142cc0b754b
Supason Kotanut <supasonk@gmail.com>
2021-05-31 22:56:34 +0700

    feat | fix css

--- src/page/topAuction/topAuction.jsx
+++ src/page/topAuction/topAuction.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect  } from 'react'
-import { PageLayout } from '../../component/pageLayout/pageLayout';
+import Navigation from '../../component/navigation'
 import { TimeBox } from '../../component/timeBox/timeBox';
 import { AuctionTable } from '../../component/auctionTable/auctionTable';
 import ReactPaginate from 'react-paginate';
 import { TopAuctionCard } from '../../component/topAuctionCard/topAuctionCard';
 import useWindowDimensions from '../../hooks/useWindowDimension';
 import { useBidData } from '../../hooks/useBidData'
+import './topAuction.css'
 
 export const TopAuction = () => {
   const { bidData } = useBidData()
@@ -13,6 +14,9 @@ export const TopAuction = () => {
   const [paginateAuctions, setPaginationAuctions] = useState([])
   const [currentPage, setCurrentPage] = useState(0)
   const [top1, setTop1] = useState({})
+  const [top2, setTop2] = useState({})
+  const [top3, setTop3] = useState({})
+  const [top4, setTop4] = useState({})
   const { height, width } = useWindowDimensions()
   const pageSize = 25
 
@@ -62,6 +66,9 @@ export const TopAuction = () => {
     })
     console.log(value.slice(0, 10))
     setTop1(value[0])
+    setTop2(value[1])
+    setTop3(value[2])
+    setTop4(value[3])
     setAuctions(value)
   }, [bidData])
 
@@ -73,8 +80,9 @@ export const TopAuction = () => {
   }, [currentPage, auctions])
   
   return (
-    <PageLayout>
+    <div className="top-auction">
        <div className="bg-primary min-h-screen flex justify-center w-screen">
+        <Navigation />
         <div className="screen-container">
           <h1 className="text-center pt-20 text-white font-semibold xl:text-6xl text-4xl">
             Auction Ending in
@@ -93,20 +101,22 @@ export const TopAuction = () => {
             Top Auction
           </h1>
           <div className="flex justify-center  w-full mt-10">
-            <TopAuctionCard number={1} auction={top1} />
+            <div className='auction-width'>
+              <TopAuctionCard number={1} auction={top1} />
+            </div>
           </div>
-          <div className="hidden xl:flex w-full justify-center mt-10 ">
+          <div className=" xl:flex row-mobile-col-desktop w-full justify-center mt-10 ">
             <div className="xl:mr-6  mb-10 xl:mb-2">
-              <TopAuctionCard number={2} />
+              <TopAuctionCard number={2} auction={top2} />
             </div>
             <div className="xl:mr-6 xl:mb-2 mb-10 ">
-              <TopAuctionCard number={3} />
+              <TopAuctionCard number={3} auction={top3} />
             </div>
             <div className="xl:mr-6 xl:mb-2 mb-10">
-              <TopAuctionCard number={4} />
+              <TopAuctionCard number={4} auction={top4} />
             </div>
           </div>
-          <div className=" xl:flex-row xl:hidden flex flex-col w-full justify-center mt-10 ">
+          {/* <div className=" xl:flex-row  flex flex-col w-full justify-center mt-10 ">
             <div className="xl:mr-6  flex justify-center mb-10 xl:mb-2">
               <TopAuctionCard number={2} />
             </div>
@@ -116,9 +126,9 @@ export const TopAuction = () => {
             <div className="xl:mr-6 xl:mb-2 mb-10 flex justify-center">
               <TopAuctionCard number={4} />
             </div>
-          </div>
+          </div> */}
           <AuctionTable auctions={paginateAuctions} />
-          <div className="justify-center pb-96 hidden xl:flex">
+          <div className="justify-center pb-96 flex">
             <ReactPaginate
               previousLabel={'<'}
               nextLabel={'>'}
@@ -131,22 +141,9 @@ export const TopAuction = () => {
               containerClassName={'pagination'}
             />
            </div>
-           <div className="flex justify-center pb-96 xl:hidden">
-            <ReactPaginate
-              previousLabel={'<'}
-              nextLabel={'>'}
-              breakLabel={'...'}
-              initialPage={0}
-              pageCount={auctions.length / pageSize}
-              onPageChange={onPageChange}
-              marginPagesDisplayed={1}
-              pageRangeDisplayed={1 }
-              containerClassName={'pagination'}
-            />
-           </div>
         </div>
       </div>
-    </PageLayout>
+    </div>
   );
 }
 
