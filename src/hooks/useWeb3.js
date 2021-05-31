7bc6d32fc573468c90e1e292e89640970c3099dd
Tanopwan <tanopwan@gmail.com>
2021-05-31 17:09:20 +0700

    switch network

--- src/hooks/useWeb3.js
+++ src/hooks/useWeb3.js
@@ -25,6 +25,30 @@ export function Web3Provider({ children }) {
   const _window = window
   const initWeb3 = useCallback(async () => {
     if (_window.ethereum) {
+      let chainId = await window.ethereum.request({ method: 'eth_chainId' })
+			if (chainId != 0x61) { // testnet
+				await window.ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [{
+            chainId: '0x61',
+            chainName: 'Binance Smart Chain (Testnet)',
+            nativeCurrency: { name: 'BNB', symbol: 'BNB', decimals: 18 },
+            rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545/'],
+            blockExplorerUrls: ['https://testnet.bscscan.com/']
+          }]
+        })
+        // window.ethereum.request({
+        //   method: 'wallet_addEthereumChain',
+        //   params: [{
+        //     chainId: '0x38',
+        //     chainName: 'Binance Smart Chain',
+        //     nativeCurrency: { name: 'BNB', symbol: 'BNB', decimals: 18 },
+        //     rpcUrls: ['https://bsc-dataseed.binance.org/'],
+        //     blockExplorerUrls: ['https://bscscan.com/']
+        //   }]
+        // })
+			}
+      
       const tmpWeb3 = new Web3(_window.ethereum);
       try {
         dispatch({
