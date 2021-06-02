804839c7ddbb6d358f7ed5df5b6c7e35200e0860
Ktsujin <beamonemore@gmail.com>
2021-06-02 21:41:46 +0700

    add home

--- src/page/index.js
+++ src/page/index.js
@@ -1,8 +1,9 @@
 
 import { CountdownPage }  from './countdown/index'
 import {LiveAuctionPage} from './liveAuction/index'
-import {HomePage}  from './home'
+import { default as InfoPage}  from './info'
+import { default as HomePage }  from './home'
 import { TopAuction } from './topAuction/topAuction'
-export {CountdownPage,LiveAuctionPage,HomePage, TopAuction}
+export {CountdownPage,LiveAuctionPage,HomePage, TopAuction, InfoPage}
 
 
