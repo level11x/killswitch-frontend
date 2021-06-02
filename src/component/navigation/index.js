860ad431a8f9605b9451de57a8b2a1e51faf9902
Tanopwan <tanopwan@gmail.com>
2021-06-02 18:15:30 +0700

    rename menu to info

--- src/component/navigation/index.js
+++ src/component/navigation/index.js
@@ -44,7 +44,7 @@ export default function Navigation() {
                 </svg>
                 </Link>
                 <div className="h-full flex items-center flex-1 justify-end space-x-lg">
-                    <Link className={`px-6 flex h-full items-center border-blue-900 ${window.location.pathname.toLocaleLowerCase().includes("unlock Rewards") ? 'border-b-4' : ''}`} to="/home">Unlock Rewards</Link>
+                    <Link className={`px-6 flex h-full items-center border-blue-900 ${window.location.pathname.toLocaleLowerCase().includes("unlock Rewards") ? 'border-b-4' : ''}`} to="/home">Info</Link>
                     <Link className={`px-6 flex h-full items-center border-blue-900 ${window.location.pathname.toLocaleLowerCase().includes("live-auction") ? 'border-b-4' : ''}`} to="/live-auction">Live Auction</Link>
                     <Link className={`px-6 flex h-full items-center border-blue-900 ${window.location.pathname.toLocaleLowerCase().includes("top-auction") ? 'border-b-4' : ''}`}to="/top-auction">Top Auction</Link>
                     {wallet && <button onClick={connectWallet} className="px-4 py-2 bg-blue-900 rounded text-white">{`${wallet.substr(0,4)}...${wallet.substring(wallet.length - 4, wallet.length)}`}</button>}
