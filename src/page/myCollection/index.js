539438d58b3d5c1a8d5d8c8b52a37e00fda5b8dd
Tanopwan <tanopwan@gmail.com>
2021-06-06 22:30:19 +0700

    add missing code

--- src/page/myCollection/index.js
+++ src/page/myCollection/index.js
@@ -402,6 +402,8 @@ export const MyCollectionPage = () => {
             .then((result) => {
                 console.log('result', result)
                 const params = new URLSearchParams()
+                params.append('wallet', wallet)
+                params.append("tokenID", isModalOpenForTokneID)
                 return axios.post("https://www.formbackend.com/f/bf68bb6574b69f0c", params, config)
             })
             .then(() => {
